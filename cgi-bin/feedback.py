a9d2b8a9535f9c4866e4c97ec17beb4d0530a393
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2020-09-17 19:08:14 +0900

    行の抽出を追加

--- cgi-bin/feedback.py
+++ cgi-bin/feedback.py
@@ -19,21 +19,36 @@ print("<body>")
 print("<h1>運転結果</h1>");
 
 # CSVデータのHTML出力
-print(log.to_html())
+print("<h2>100～119番目のレコード</h2>")
+place_A = log[100:120]
+print(place_A.to_html())
 
-# 視線データの抽出
-eye = log.loc[:, ["focus_x", "focus_y"]]
-#print(eye.to_html())
-
-# 視線データをグラフ化
+# place_Aの視線データの抽出
+eye_A = place_A.loc[:, ["focus_x", "focus_y"]]
 plt.figure()
-eye.plot.scatter(x="focus_x", y="focus_y")
-plt.savefig("fig/graph.png")
+eye_A.plot.scatter(x="focus_x", y="focus_y")
+plt.savefig("fig/graph_A.png")
 plt.close("all")
 
-# グラフのHTML出力
-print("<div>")
-print("<img src=" + "'../fig/graph.png'" + " >")
+# place_Aのグラフを出力
+print("<div style='text-align:center'>")
+print("<img src=" + "'../fig/graph_A.png'" + " >")
+print("</div>")
+
+print("<h2>X座標が7300以上かつ7350以下のレコード</h2>")
+place_B = log[(log.x >= 7300) & (log.x <= 7350)]
+print(place_B.to_html())
+
+# place_Bの視線データの抽出
+eye_B = place_B.loc[:, ["focus_x", "focus_y"]]
+plt.figure()
+eye_B.plot.scatter(x="focus_x", y="focus_y")
+plt.savefig("fig/graph_B.png")
+plt.close("all")
+
+# place_Bのグラフを出力
+print("<div style='text-align:center'>")
+print("<img src=" + "'../fig/graph_B.png'" + " >")
 print("</div>")
 
 print("</body>")
